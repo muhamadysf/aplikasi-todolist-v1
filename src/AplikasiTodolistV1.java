91fa962ae26971a33a6231d3285c20e7a1450c12
Muhamad Yusup <mhd.ysf0124@gmail.com>
2023-10-10 17:11:53 +0700

    penambahan kerangka

--- src/AplikasiTodolistV1.java
+++ src/AplikasiTodolistV1.java
@@ -1,5 +1,38 @@
 public class AplikasiTodolistV1 {
+
+    public static String[] model = new String[];
     public static void main(String[] args) {
+
+    }
+
+//  menampilkan isiTodolist
+    public static void showTodolist(){
+
+
+    }
+
+//  menambah todolist
+    public static void addTodolist(){
+
+    }
+
+//  menghapus todolist
+    public static void removeTodolist(){
+
+    }
+
+//  tampilan isitodolist
+    public static void viewShowTodolist(){
+
+    }
+
+//  tampilan tambah todolist
+    public static void viewAddTodolist(){
+
+    }
+
+//  tampilan hapus todolist
+    public static void viewRemoveTodolist(){
 
     }
 }
